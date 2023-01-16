213aeb44adb0992124914a474639acd198d71b5d
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-16 17:17:27 -0600

    Add solution J and K

--- Solutions part 1.js
+++ Solutions part 1.js
@@ -113,4 +113,44 @@ let inputHb = "cab";
 let regexI = /(abc)/g;
 let inputIa = "I am learning the abc";
 
-console.log(inputIa.match(regexI));
+// console.log(inputIa.match(regexI));
+
+// J.   [a-zA-Z_\$\.]+[A-Za-z_\$0-9\.]*@[a-zA-Z_\$\.]+[a-zA-Z_\$0-9\.]*\.(com|net|org){1}
+// To solve this problem it is useful to divide it by parts:
+// • PART 1: [a-zA-Z_\$\.]+ means: “At least 1 letter (+), underscore, dollar sing or dot”
+// • PART 2: [A-Za-z_\$0-9\.]* means: “an optional (*) letter (any), underscore, dollar sing, digit or dot ”
+// • PART 3: @ is a symbol with no special meaning (in regex context).
+// • PART 4: [a-zA-Z_\$\.]+  means the same as PART 1.
+// • PART 5: [a-zA-Z_\$0-9\.]* means the same as PART 2.
+// • PART 6: \. Means “.”
+// • PART 7: (com|net|org){1} The | (pipe) operator defines OR. This structure means: match “com” or “net” or “org” one time.
+// •	By using all the parts it is possible to construct some strings that will match the regex, for example:
+// -   “my_name@my$ite.com”
+// -   _$@my$AAz..$.org
+// Example:
+let regexJ =
+  /[a-zA-Z_\$\.]+[A-Za-z_\$0-9\.]*@[a-zA-Z_\$\.]+[a-zA-Z_\$0-9\.]*\.(com|net|org){1}/g;
+let inputJa = "my_name@my$ite.com";
+let inputJb = "_$@my$AAz..$.org";
+
+// console.log(inputJa.match(regexJ));
+// console.log(inputJb.match(regexJ));
+
+// K.	\([0oOn]{1}(_|\s)[0oOn]{1}\)
+// As before, it is useful to divided the problem in parts
+// • PART 1: \( means: “(”
+// • PART 2: [0oOn]{1} means: match one and only one: “0”, or “o”, or “O”, or “n”
+// • PART 3: (_|\s) means: match a “_” or a space
+// • PART 4: [0oOn]{1} means the same as PART 2
+// • PART 5: \) means “)”
+// •	By using all the parts, it is possible to construct some strings that match the regex, for example:
+// - “(o_n)”
+// - “(o O)”
+// Example:
+let regexK = /\([0oOn]{1}(_|\s)[0oOn]{1}\)/g;
+let inputKa = "(o_n)";
+let inputKb = "(o O)";
+
+console.log(inputKa.match(regexK));
+console.log(inputKb.match(regexK));
+
