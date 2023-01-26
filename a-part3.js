56c928aad6cbfe720f83252bf711387386c4963a
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-01-26 11:41:24 -0600

    Feedback
    3.A) Ceasar’s cipher
    - you only need to use a single regular expression and a method associated with them (replace)

--- a-part3.js
+++ a-part3.js
@@ -2,61 +2,38 @@ function cicleShift(inputString) {
   //-------------START: BASIC DEFINITIONS-----------------
   // To organize the letters and the numbers
   const letters = "abcdefghijklmnopqrstuvwxyz";
-  const digits = "0123456789";
 
   // These will be used to make the cicle,
   // they are the length of the letters and
   // the digits, respectively
   let lettersNorm = 26;
-  let digitsNorm = 10;
-
-  // Variable in which the output will be saved
-  let output = "";
   //-------------END: BASIC DEFINITIONS-----------------
 
-  //-------------START: ITERATION-----------------
-  // This for iterares over the elements of the input
-  for (let character of inputString) {
-    // A character in the input have 4 possible options
-    // 1) It is a capital letter.
-    // 2) It is a lower case letter.
-    // 3) it is a digit.
-    // 4) It is non of the previous 3.
-    // I have to say that 4) was not in the problem, but I think
-    // it is important to consider characters such as */?.&%$#,
-    // this, in order to make the code work in more situations.
+  // --Start: newString--
+  var newString = inputString.replace(/[a-z0-9]/gi, function (match) {
+    if (match.match(/[a-z]/gi)) {
+      let lowerMatch = match.toLowerCase();
+      let index = letters.indexOf(lowerMatch);
+      let output = letters[(index + 1) % lettersNorm];
 
-    // 1) Is the character a capital letter?
-    if (/[A-Z]/.test(character)) {
-      let index = letters.indexOf(character.toLowerCase());
-      output += letters[(index + 1) % lettersNorm].toUpperCase();
+      if (match === lowerMatch) {
+        //*letter is lowercase*
+        return output.toUpperCase();
+      } else {
+        //*letter is uppercase*
+        return output.toLowerCase();
+      }
+    } else if (match.match(/[0-9]/)) {
+      if (match === "9") {
+        return "0";
+      } else {
+        return String(Number(match) + 1);
+      }
     }
-    // 2) Is the character a lower case letter?
-    else if (/[a-z]/.test(character)) {
-      let index = letters.indexOf(character);
-      output += letters[(index + 1) % lettersNorm];
-    }
-    // 3) Is the character a digit?
-    else if (/[0-9]/.test(character)) {
-      let index = digits.indexOf(character);
-      output += digits[(index + 1) % digitsNorm];
-    }
-    // 4) Is the character something else?
-    else {
-      output += character;
-    }
-  }
-  //-------------END: ITERATION-----------------
+  });
+  // ---End: newString---
 
-  //Note:
-  // The structure: -letters[(index + 1) % lettersNorm]- makes it possible
-  // to make the cicle, for example, for the letter -z-, -indexOf(z) = 25-,
-  // thus it is not possible to obtain -letters[25 + 1]- because
-  // -letters[26]- does not exists, neverthless,
-  // 26 % lettersNorm = 26%26 = 0, and -letter[0] = a-.
-  // by using -%- it is possible to make the cicle.
-
-  return output;
+  return newString;
 }
 
 const inpt1 = "aBc";
