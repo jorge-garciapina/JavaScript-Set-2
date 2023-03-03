bbd1f0747eeaafc9b2f5c7e742df56885a92267c
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-03-03 17:38:05 -0600

    Feedback

--- a-part3.js
+++ a-part3.js
@@ -1,49 +1,47 @@
 function cicleShift(inputString) {
-  //-------------START: BASIC DEFINITIONS-----------------
-  // To organize the letters and the numbers
-  const letters = "abcdefghijklmnopqrstuvwxyz";
+  // Define the alphabet and its length
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetLength = alphabet.length;
 
-  // These will be used to make the cicle,
-  // they are the length of the letters and
-  // the digits, respectively
-  let lettersNorm = 26;
-  //-------------END: BASIC DEFINITIONS-----------------
+  // Replace each letter and number with its shifted value
+  const shiftedString = inputString.replace(/[a-z0-9]/gi, function (match) {
+    // Get the Unicode character code value of the current character
+    let charCode = match.charCodeAt(0);
 
-  // --Start: newString--
-  var newString = inputString.replace(/[a-z0-9]/gi, function (match) {
-    if (match.match(/[a-z]/gi)) {
-      let lowerMatch = match.toLowerCase();
-      let index = letters.indexOf(lowerMatch);
-      let output = letters[(index + 1) % lettersNorm];
-
-      if (match === lowerMatch) {
-        //*letter is lowercase*
-        return output.toUpperCase();
-      } else {
-        //*letter is uppercase*
-        return output.toLowerCase();
-      }
-    } else if (match.match(/[0-9]/)) {
-      if (match === "9") {
-        return "0";
-      } else {
-        return String(Number(match) + 1);
-      }
+    // Check if the character is a letter
+    if (/[a-z]/gi.test(match)) {
+      // Apply the cyclic shift to the letter
+      let shiftedCharCode = ((charCode - 97 + 1) % alphabetLength) + 97;
+      // Convert the shifted code point back to a character and return it
+      return String.fromCharCode(shiftedCharCode);
+    }
+    // Check if the character is a number
+    else if (/[0-9]/.test(match)) {
+      // Apply the cyclic shift to the number
+      let shiftedCharCode = ((charCode - 48 + 1) % 10) + 48;
+      // Convert the shifted code point back to a character and return it
+      return String.fromCharCode(shiftedCharCode);
+    }
+    // For any other character, just return it unchanged
+    else {
+      return match;
     }
   });
-  // ---End: newString---
 
-  return newString;
+  // Return the final shifted string
+  return shiftedString;
 }
 
 const inpt1 = "aBc";
-const inpt2 = "xyz";
-const inpt3 = "aK89";
-const inpt4 = "A9B8c+*/7d6";
-
 const result1 = cicleShift(inpt1);
+
+const inpt2 = "xyz";
 const result2 = cicleShift(inpt2);
+
+const inpt3 = "aK89";
 const result3 = cicleShift(inpt3);
+
+const inpt4 = "A9B8c+*/7d6";
 const result4 = cicleShift(inpt4);
 
 console.log(result1);
