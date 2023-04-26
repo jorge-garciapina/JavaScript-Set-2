2ddc2784434b224a38dc52d7607ae9cd9b29f8a9
jorge-garciapina <115723820+jorge-garciapina@users.noreply.github.com>
2023-04-26 11:30:01 -0600

    Feedback April

--- b-part3.js
+++ b-part3.js
@@ -6,22 +6,20 @@ const mainText = document.getElementById("main-text");
 let texto = mainText.textContent;
 //---------------END: INITIALIZATION-------------------
 
-
 //-------------START: APPEND HASH-----------------
 function submit() {
   // Every time the function is called, the code sets
   // the value of the text, to its original. This
-  // avoids have multiple hashed words.
-  mainText.textContent = texto
+  // avoids having multiple hashed words.
+  mainText.textContent = texto;
 
   // Extraction of the word given by the user
   let word = document.getElementsByName("main-word");
   const toHashTag = word[0].value;
 
-  // Validation of the input 
+  // Validation of the input
   // The validation is: only letters between word boundaries:
-  if(/\b[a-z]+\b/ig.test(toHashTag)){
-    
+  if (/^\b[a-zA-Z]+\b$/i.test(toHashTag)) {
     // The word that the code searches is between word
     // boundaries, this is to avoid problems such as:
     // if word = ball, then the result will be Base#ball
@@ -29,23 +27,20 @@ function submit() {
     const regex = new RegExp(`\\b${toHashTag}\\b`, "gi");
     const twitter = `https://twitter.com/search?q=${toHashTag}`;
 
-    // Serch for the word given by the user:
-    // If the word is found, a # is append at the begging
+    // Search for the word given by the user:
+    // If the word is found, a # is append at the beginning
     let replacement = mainText.textContent.replace(
       regex,
       `<a href="${twitter}">#${toHashTag}</a>`
     );
-    
-    // The code refresh the content of the text:
-    mainText.innerHTML = replacement
-  }else{
-    alert("Introduce a valid word")
+
+    // The code refreshes the content of the text:
+    mainText.innerHTML = replacement;
+  } else {
+    alert("Introduce a valid word");
   }
-  
 }
+
 //--------------END: APPEND HASH------------------
-
-
-
 
 
